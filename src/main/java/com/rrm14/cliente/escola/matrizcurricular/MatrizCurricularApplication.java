dd11b3917a238b1f852f377fb7ed5b853c9ab42d
moduretick <robertoreboucasdemelo@gmail.com>
2021-08-15 05:20:11 -0300

    Versao 0.1

---
+++ src/main/java/com/rrm14/cliente/escola/matrizcurricular/MatrizCurricularApplication.java
@@ -0,0 +1,13 @@
+package com.rrm14.cliente.escola.matrizcurricular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatrizCurricularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatrizCurricularApplication.class, args);
+	}
+
+}
